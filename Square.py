f0273a8a03205aabcffc6051831bd3954de311ed
Brian McRoberts <brimcrob@gmail.com>
2020-02-24 18:38:17 +0000

    First square

---
+++ Square.py
@@ -0,0 +1,13 @@
+import turtle
+import time
+
+my_turtle = turtle.Turtle()
+my_turtle.forward(100)
+my_turtle.left(90)
+my_turtle.forward(100)
+my_turtle.left(90)
+my_turtle.forward(100)
+my_turtle.left(90)
+my_turtle.forward(100)
+# to keep the window open for a bit longer to view it
+time.sleep(5)
